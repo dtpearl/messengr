c7331a46e603374a948c15bbb86eea7f90c04676
David Pearl <davidtpearl@gmail.com>
2019-03-04 14:05:56 +1100

    Added Redux for mananging state. Building out App component - SidePanel.

--- src/index.js
+++ src/index.js
@@ -3,23 +3,57 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
+import Spinner from './Spinner';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
+import firebase from './firebase';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+import { setUser } from './actions';
+
 
 import "semantic-ui-css/semantic.min.css";
 
-const Root = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={ App } />
-      <Route path="/login" component={ Login } />
-      <Route path="/register" component={ Register } />
-    </Switch>
-  </Router>
-);
+const store = createStore( rootReducer, composeWithDevTools());
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+class Root extends React.Component {
+  componentDidMount() {
+    console.log( this.props.isLoading );
+    firebase.auth().onAuthStateChanged( user => {
+      if ( user ) {
+        this.props.setUser(user);
+        this.props.history.push('/');
+      }
+    })
+  }
 
+  render () {
+    return this.props.isLoading ? <Spinner /> : (
+        <Switch>
+          <Route exact path="/" component={ App } />
+          <Route path="/login" component={ Login } />
+          <Route path="/register" component={ Register } />
+        </Switch>
+    );
+  }
+}
+
+const mapStateFromProps = (state) => {
+  return ({
+    isLoading: state.user.isLoading
+  });
+}
+
+const RootWithAuth = withRouter(connect( mapStateFromProps, { setUser })(Root));
+
+ReactDOM.render(
+  <Provider store={ store }>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
